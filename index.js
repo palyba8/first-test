48d2c4fdb3805f7fb32cc4f859120df68acca8ae
Serhiy Paluba <palyba8@gmail.com>
2022-04-30 23:13:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function createPassword() {
         "!", "@", "#", "$", "%", "^", "&", "*", ">", "<",
         "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-    if (inputLength.value) { passwordLength = inputLength.value -1 }
+    if (inputLength.value) { passwordLength = (inputLength.value - 1) }
 
     for (let i = 0; i <= passwordLength; i++) {
 
